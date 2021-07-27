dd7659e1562d4faf88c75407150a03c4f4b997c8
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-07-27 13:07:44 +0100

    Add views in authentications pages

---
+++ src/resources/auth/auth.cms.js
@@ -0,0 +1,41 @@
+const content = {
+  en: {
+    login: {
+      title: 'Login Page',
+      description: 'Activate vonting account for Employees',
+      pageTitle: 'Login to your account',
+      usernameLabel: 'Username',
+      emailLabel: 'Email',
+      passwordLabel: 'Password',
+      loggedIn: 'You are already logged in!',
+      forgotPassword: 'Forgot password?',
+      submitBtn: 'Connect',
+      resetBtn: 'Reset',
+      meta: {
+        path: '/login',
+        parentPath: '/',
+      },
+    },
+    signup: {
+      title: 'Sign up Page',
+      description: 'Activate vonting account for Employees',
+      pageTitle: 'Activate your account',
+      usernameLabel: 'Username',
+      emailLabel: 'Email',
+      passwordLabel: 'Password',
+      birthdateLabel: 'Birthdate',
+      cinLabel: 'CIN',
+      resetBtn: 'Reset',
+      submitBtn: 'Sign up',
+      meta: {
+        path: '/signup',
+        parentPath: '/',
+      },
+    },
+    meta: {
+      appName: 'Evote App',
+    },
+  },
+};
+
+export default content;
