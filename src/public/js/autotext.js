2c991bb79112165b4ceb238631d5988a2b8a0b3e
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-07-28 02:34:03 +0100

    Update home page code, fix js syntax

--- src/public/js/autotext.js
+++ src/public/js/autotext.js
@@ -1,56 +1,68 @@
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+class TxtRotate {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+  run() {
+    this.tick();
     this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
   }
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+  tick() {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+    const that = this;
+    let delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum += 1;
+      delta = 500;
+    }
+
+    setTimeout(() => {
+      that.tick();
+    }, delta);
+  }
+}
+
+window.addEventListener('load', () => {
+  const elements = document.getElementsByClassName('txt-rotate');
+  for (let i = 0; i < elements.length; i++) {
+    const toRotate = elements[i].dataset.rotate;
+    const { period } = elements[i].dataset;
     if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      const txtRotate = new TxtRotate(
+        elements[i],
+        JSON.parse(toRotate),
+        period,
+      );
+      txtRotate.run();
     }
   }
   // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  const css = document.createElement('style');
+  css.setAttribute('type', 'text/css');
+  css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
   document.body.appendChild(css);
-};
+});
