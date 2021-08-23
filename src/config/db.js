0709ff0d85f994d1731520e1f058f44e68457fab
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-08-23 11:44:26 +0100

    Update connect log using chalk for output colors

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-console */
+import chalk from 'chalk';
 import mongoose from 'mongoose';
 
 import Config from './config';
 
 export const connectDB = (options = {}, url = Config.DB_URL) =>
-  mongoose.connect(url, {
-    ...options,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  mongoose
+    .connect(url, {
+      ...options,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then((conn) => {
+      const connected = chalk.yellowBright('MongoDB Connection established');
+      const { host, port, name } = conn.connections[0];
+      const conString = chalk.bold(`${name}@${host}:${port}`);
+      console.log();
+      console.log('>', `${connected} on ${conString}`);
+      console.log();
+      return conn;
+    })
+    .catch((e) => console.log('>', chalk.red(`MongoDB Connection error `, e)));
